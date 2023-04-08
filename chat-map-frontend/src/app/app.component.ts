2683046d623105f9d3b3af0a7a9197d586525bf7
Kevin <k_keller29@aol.com>
2023-04-07 19:37:29 -0500

    added back-end and organized file structure

---
+++ chat-map-frontend/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // private httpClient: HttpClient;
+  title = 'ChatMap';
+
+  refObj: any = [];
+  ajaxObj: any = {};
+
+  latitude = 51.678418;
+  longitude = 7.809007;
+  // constructor(httpClient: HttpClient){
+  //   this.httpClient = httpClient;
+  // }
+  // getRequest() {
+  //   try {
+  //     console.log(new Date());
+
+  //     let url = "http://gordon.cs.uwec.edu:8080/CS355/Group3/";
+  
+  //     // HTTP GET REQUEST :: AJAX REQUEST
+  
+  //     let obs1 = this.httpClient.get(url);
+  //     obs1.subscribe((serverData) => {
+  //       console.log(serverData);
+  //     });
+
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+}
