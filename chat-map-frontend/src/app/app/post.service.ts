5c793e9479b0e4bbfc2d9c7a142e50549ae10da7
prestoscott <126613551+prestoscott@users.noreply.github.com>
2023-05-06 16:58:09 -0500

    Post Service

--- chat-map-frontend/src/app/app/post.service.ts
+++ chat-map-frontend/src/app/app/post.service.ts
@@ -1,5 +1,14 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+
+
+export interface Post {
+  title:string;
+  content:string;
+  latitude:number;
+  longitude:number;
+  userId:number;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +17,17 @@ export class PostService {
 
   constructor(public httpClient: HttpClient) { }
 
+
+
   createPost(title:string, message:string, lat:number, lng:number, userId:number){
     console.log(title,message,lat,lng,userId);
-    return this.httpClient.post<any>('http://localhost:3000/post',{title,message,lat,lng,userId});
+    const post: Post = {
+      title: title,
+      content: message,
+      latitude: lat,
+      longitude: lng,
+      userId: 3
+    };
+    return this.httpClient.post<any>('http://localhost:3000/post',{post});
   }
 }
