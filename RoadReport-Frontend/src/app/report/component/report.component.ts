c83bdf1ed22ec5ce879b66304a0a401e9fdda427
devonwinquist <devonwinquist14@gmail.com>
2023-11-29 14:43:49 -0600

    Additional login, register, and report styling added, frontend aspect of image functionality added

--- RoadReport-Frontend/src/app/report/component/report.component.ts
+++ RoadReport-Frontend/src/app/report/component/report.component.ts
@@ -1,9 +1,10 @@
 import { LoginService } from '../../../services/login.service';
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import { ReportService } from 'src/services/report.service';
 import { map, tap } from 'rxjs';
+
 
 @Component({
   selector: 'app-report',
@@ -18,13 +19,20 @@ export class ReportComponent implements OnInit{
   latitude: number;
   longitude: number;
   userId: number;
+  image: string;
 
-  constructor(private fb: FormBuilder, private dialogRef: MatDialogRef<ReportComponent>, @Inject(MAT_DIALOG_DATA) data: any, public reportService: ReportService, public loginService: LoginService) {
+  @ViewChild('fileInput', { static: false }) fileInput: ElementRef;
+  isDraggedOver: boolean = false;
+  selectedImage: File | null = null;
+  previewUrl: string | ArrayBuffer | null = null;
+
+  constructor(private fb: FormBuilder, private dialogRef: MatDialogRef<ReportComponent>, @Inject(MAT_DIALOG_DATA) data: any, public reportService: ReportService, public loginService: LoginService, private renderer: Renderer2) {
     this.title = data.title;
     this.content = data.message;
     this.latitude = data.latitude;
     this.longitude = data.longitude;
     this.userId = data.userId;
+    this.image = data.image;
   }
 
   ngOnInit(): void {
@@ -36,9 +44,54 @@ export class ReportComponent implements OnInit{
       content: [this.content,[]],
       latitude: [this.latitude,[]],
       longitude: [this.longitude,[]],
-      userId: [this.userId, []]
+      userId: [this.userId, []],
+      file: [null],
     });
   }
+
+  onFileSelected(event: any): void {
+    const files = event.target.files;
+    if (files.length > 0) {
+      this.selectedImage = files[0];
+      this.updatePreview();
+    }
+  }
+
+  updatePreview(): void {
+    if (this.selectedImage) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.previewUrl = reader.result;
+        console.log('Preview URL:', this.previewUrl);
+      };
+      reader.readAsDataURL(this.selectedImage);
+    }
+  }
+
+  getImageUrl(): string | null {
+    if (this.selectedImage) {
+      return URL.createObjectURL(this.selectedImage);
+    }
+    return null;
+  }
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDraggedOver = true;
+}
+
+onFileDropped(event: DragEvent): void {
+  event.preventDefault();
+  event.stopPropagation();
+
+  const files = event.dataTransfer?.files;
+  if (files && files.length > 0) {
+    this.selectedImage = files[0];
+    this.updatePreview();
+    this.form.get('file')?.setValue(this.selectedImage);
+  }
+}
+
 
   submitReport() {
     this.dialogRef.close(this.form.value);
